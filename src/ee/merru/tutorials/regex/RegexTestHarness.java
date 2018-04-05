25351b87229608c5727ec31d1356f6a9d2f3b79e
MerilyR <merily.r@gmail.com>
2018-04-05 22:56:00 +0300

    matches predefined patterns

--- src/ee/merru/tutorials/regex/RegexTestHarness.java
+++ src/ee/merru/tutorials/regex/RegexTestHarness.java
@@ -8,7 +8,11 @@ import java.util.regex.PatternSyntaxException;
 public class RegexTestHarness {
 	public static void main(String[] args){
         Pattern pattern = null;
+        Pattern pattern1 = Pattern.compile("\\d{4}-\\d{2}-\\d{2}\\s+\\d{2}:\\d{2}:\\d{2},\\d{3}\\s+\\((.+)\\)\\s+\\[[^\\]]*\\]\\s+([\\S])+\\s+in\\s+\\d+");
+        Pattern pattern2 = Pattern.compile("\\d{4}-\\d{2}-\\d{2}\\s+\\d{2}:\\d{2}:\\d{2},\\d{3}\\s+\\((.+)\\)\\s+\\[[^\\]]*\\]\\s+(\\w)+(\\s+([\\S])+)*\\s+in\\s+\\d+");
         Matcher matcher = null;
+        Matcher matcher1 = null;
+        Matcher matcher2 = null;
 
         Console console = System.console();
         if (console == null) {
@@ -17,11 +21,49 @@ public class RegexTestHarness {
         }
         while (true) {
             try{
-                pattern = 
-                Pattern.compile(console.readLine("%nEnter your regex: "));
-
-                matcher = 
-                pattern.matcher(console.readLine("Enter input string to search: "));
+            	String s = console.readLine("%nUse predefined regex (y) or enter your own (n)?");
+            	boolean choice = 
+            	s.equalsIgnoreCase("true") || s.equalsIgnoreCase("t") || 
+                s.equalsIgnoreCase("yes") || s.equalsIgnoreCase("y");
+            	
+            	if (!choice) {
+            		pattern = 
+            		Pattern.compile(console.readLine("%nEnter your regex: "));
+	
+	                matcher = 
+	                pattern.matcher(console.readLine("%nEnter input string to search: "));
+	                
+	                boolean found = false;
+	                while (matcher.find()) {
+	                    console.format("I found the text" +
+	                        " \"%s\" starting at " +
+	                        "index %d and ending at index %d.%n",
+	                        matcher.group(),
+	                        matcher.start(),
+	                        matcher.end());
+	                    found = true;
+	                }
+	                if(!found){
+	                    //console.format("No match found.%n");
+	                	console.format ("Input string doesn't match !?!%n");
+	                }
+	                
+            	}
+            	else {
+            		int lines = Integer.parseInt(console.readLine("%nEnter how many lines you want to check: "));
+            		
+            		for (int i = 0; i < lines; i++) {
+            			String searchString = console.readLine("%nEnter input string to check: ");
+            			matcher1 = pattern1.matcher(searchString);
+            			matcher2 = pattern2.matcher(searchString);
+            			if (matcher1.find())
+            				console.format("Line matches Pattern 1%n");
+            			if (matcher2.find())
+            				console.format("Line matches Pattern 2%n");
+            			console.format("Line "+i+1+" checked!%n%n");
+            		}
+            		
+            	}
             }
             catch(PatternSyntaxException pse){
                 console.format("There is a problem" +
@@ -35,21 +77,6 @@ public class RegexTestHarness {
                 console.format("The index is: %s%n",
                                pse.getIndex());
                 System.exit(0);
-            }
-            boolean found = false;
-            while (matcher.find()) {
-                /*console.format("I found the text" +
-                    " \"%s\" starting at " +
-                    "index %d and ending at index %d.%n",
-                    matcher.group(),
-                    matcher.start(),
-                    matcher.end());*/
-            	console.format("Input string matches the regex :)");
-                found = true;
-            }
-            if(!found){
-                //console.format("No match found.%n");
-            	console.format ("Input string doesn't match !?!");
             }
         }
     }
